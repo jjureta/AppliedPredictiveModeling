cc370049d20d0536f9b5b463f2ca8ee1ddb3badb
jjureta <josip.jureta@gmail.com>
2016-06-11 17:05:11 -0400

    exercice 3.1

--- exercices/3/exercice_3.1.R
+++ exercices/3/exercice_3.1.R
@@ -1,3 +1,30 @@
 library(mlbench)
+library(e1071)
+library(reshape2)
 
 data(Glass)
+
+# Remove Type column
+glassSubset <- subset(Glass, select = -Type)
+skewValues <- apply(glassSubset, 2, skewness)
+
+glassMelt <- melt(glassSubset)
+
+plot1 <- ggplot(data=glassMelt, aes(glassMelt$value)) + 
+  geom_freqpoly(bins = 200) +
+  facet_wrap(~variable, scales = "free") 
+
+print(plot1)
+
+#plot2 <- densityplot(~value|variable,
+#            data = glassMelt,
+#            ## Adjust each axis so that the measurement scale is
+#            ## different for each panel
+#            scales = list(x = list(relation = "free"),
+#                          y = list(relation = "free")),
+#            ## 'adjust' smooths the curve out
+#            adjust = 1.25,
+#            ## change the symbol on the rug for each data point
+#            pch = "|",
+#            xlab = "Predictor")
+#print(plot2)
